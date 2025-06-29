c0e758edfbbfd891e589c1a9797efed061b05272
anotherSimpleCoder <amsakan@posteo.de>
2025-06-29 18:37:22 +0200

    Added code for increment 1 (Red and Green LED)

---
+++ code/increment1/code.ino
@@ -0,0 +1,26 @@
+const int RED_LED_PIN = 10;
+const int GREEN_LED_PIN = 11;
+
+void redBlink() {
+  digitalWrite(RED_LED_PIN, HIGH);
+  delay(2000);
+  digitalWrite(RED_LED_PIN, LOW);
+  delay(2000);
+}
+
+void greenBlink() {
+  digitalWrite(GREEN_LED_PIN, HIGH);
+  delay(2000);
+  digitalWrite(GREEN_LED_PIN, LOW);
+  delay(2000);
+}
+
+void setup() {
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+}
+
+void loop() { 
+  redBlink();
+  greenBlink();
+}
