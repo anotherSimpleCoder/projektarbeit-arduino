961d16b724865a06dec0756c6c1559f483a49d13
Amsakan Bavan <amsakan.bavan@softwareag.com>
2025-08-04 12:34:37 +0200

    Implemented PIN logic

---
+++ old/code/Increment2/String.hh
@@ -0,0 +1,17 @@
+namespace Strings {
+  bool equals(const char* string1, const char* string2) {    
+      for(int i = 0; i < 4; i++) {
+        if(string1[i] != string2[i]) {
+          return false;
+        }
+      }
+
+      return true;
+  }
+
+  void clear(char* string) {
+    for(int i = 0; i < 4; i++) {
+      string[i] = '\0';
+    }
+  }
+};
--- old/code/Increment2/increment2.ino
+++ old/code/Increment2/increment2.ino
@@ -1,9 +1,14 @@
 #include "Leds.hh"
 #include "Keys.hh"
+#include "String.hh"
 
 const Keypad* keypad;
+char pinCode[4] = "1473";
+char buffer[4];
+int i = 0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LEDS::RED_LED_PIN, OUTPUT);
   pinMode(LEDS::GREEN_LED_PIN, OUTPUT);
   keypad = Keys::newKeypad();
@@ -14,6 +19,19 @@ void loop() {
   if(!key) return;
 
   if(key == '#') {
-    LEDS::greenBlink(); 
+    bool equal = Strings::equals(pinCode, buffer);
+    
+    if(equal) {
+      LEDS::greenBlink();
+    } else {
+      LEDS::redBlink();
+    }
+
+    i = 0;
+    Strings::clear(buffer);
+  } else if(i < 4) {
+    Serial.println(key);
+    buffer[i] = key;
+    i++;
   }
 }
